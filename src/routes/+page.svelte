83ce798a897ecc8c968c57254b97b50097f441f2
Tommaso Prinetti <Tommi.prinetti@gmail.com>
2025-11-05 14:23:00 +0100

    added sample file + fix navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
     import PdfViewer from '$lib/PdfViewer.svelte';
+    import sampleLocal from '$lib/sample-local-pdf.pdf';
 </script>
 
-<PdfViewer url='./sample.pdf' />
+<PdfViewer
+    url={sampleLocal}
+    twoPage={true}
+    showButtons={[ 'navigation', 'zoom', 'rotate', 'pageInfo', 'timeInfo', 'download', 'print', 'autoflip' ]}
+    showBorder={true}
+    showTopButton={true}
+/>
